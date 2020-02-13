80c1f47b22b7d40e617678833205c6179fd9314b
Justin Moore <juswmoore@gmail.com>
2020-02-13 05:05:54 -0800

    Plus One:
    Given a non-empty array of digits representing a non-negative
    integer, plus one to the integer.
    
    Time complexity is O(n)

--- arrays/easy/plus_one.py
+++ arrays/easy/plus_one.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
 
 class Solution:
-    def plus_one(self, digitis: list):
+    def plus_one(self, digits: list) -> int:
+        digits[-1] += 1
+        for i in range(len(digits)-1, 0, -1):
+            if digits[i] != 10:
+                break
+            digits[i] = 0
+            digits[i-1] += 1
+
+        if digits[0] == 10:
+            digits[0] = 0
+            return [1] + digits
+        
+        return digits
+            
--- arrays/easy/test/test_plus_one.py
+++ arrays/easy/test/test_plus_one.py
@@ -9,15 +9,23 @@ class TestPlusOne(unittest.TestCase):
 
 
     def test_leet_one(self):
-        assertEqual(self.solution.plus_one([1,2,3]), [1,2,4])
+        self.assertEqual(self.solution.plus_one([1,2,3]), [1,2,4])
     
     
     def test_leet_two(self):
-        assertEqual(self.solution.plus_one([4,3,2,1]), [4,3,2,2])
+        self.assertEqual(self.solution.plus_one([4,3,2,1]), [4,3,2,2])
 
 
     def test_mine_one(self):
-        assertEqual(self.solution.plus([9]), [1,0])
+        self.assertEqual(self.solution.plus_one([9]), [1,0])
+
+    
+    def test_mine_two(self):
+        self.assertEqual(self.solution.plus_one([4,3,9]), [4,4,0])
+
+    
+    def test_mine_three(self):
+        self.assertEqual(self.solution.plus_one([10,2]), [1,0,3])
 
 
 if __name__ == "__main__":
