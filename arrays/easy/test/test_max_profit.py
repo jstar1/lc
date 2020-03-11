405c46f04612ce2b5fd5156acad3bb7729db1f51
Justin Moore <juswmoore@gmail.com>
2020-03-11 04:18:36 -0700

    max_profit: Add test

--- arrays/easy/test/test_max_profit.py
+++ arrays/easy/test/test_max_profit.py
@@ -10,6 +10,9 @@ class TestMaxProfit(unittest.TestCase):
     
     def test_lc_one(self):
         self.assertEqual(self.solution.max_profit([7,1,5,3,6,4]), 5)
+    
+    def test_lc_two(self):
+        self.assertEqual(self.solution.max_profit([7, 6, 4, 3, 1]), 0)
 
 if __name__ == '__main__':
     unittest.main()
